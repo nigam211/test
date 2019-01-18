4b2de1ef51e9396756bb1cddd684c3f6672520e1
nigam <nigampatra092@gmail.com>
2019-01-18 11:13:05 +0530

    sop statement add

--- Test.java
+++ Test.java
@@ -1,6 +1,11 @@
 class Test{
 public static void main(String[] args) {
 System.out.println("nigam");
+System.out.println("java");
+System.out.println("java");
+
+System.out.println("java1");
+}
 
 		}
 }
